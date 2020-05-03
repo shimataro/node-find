5d91e24f7b3b3763fcd0da9205c7fb0df44fe4e3
shimataro <shimataro@zelkova.cc>
2020-05-04 08:26:13 +0900

    fix filename

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import "core-js/modules/es7.symbol.async-iterator";
+import "core-js/modules/es.symbol.async-iterator";
 
 import {parseArguments} from "./parser";
 import {findEntries} from "./finder";
