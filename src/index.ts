c457965d91c01d6ef1485ee5cbff25d994f58ca9
shimataro <shimataro@zelkova.cc>
2020-05-09 22:27:21 +0900

    fix parameters

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ async function main(argv: string[]): Promise<void>
 	{
 		for(const exec of options.exec)
 		{
-			tasks.push(execCommand(exec, entry.toString()));
+			tasks.push(execCommand(exec, entry.toString(), options.startingPoint));
 			if(tasks.length >= options.concurrency)
 			{
 				await Promise.all(tasks);
